d86a2231638a51dd67ee55816b3c90a16698b341
hairstonm <matthairston.79@gmail.com>
2014-09-02 14:03:32 -0400

    making changes

--- grails-app/domain/grailsblog/Commentator.groovy
+++ grails-app/domain/grailsblog/Commentator.groovy
@@ -1,7 +1,18 @@
 package grailsblog
 
 class Commentator {
+	
+	static belongsTo = Comment
 
-    static constraints = {
-    }
+	String name
+	String url
+	String email
+	Comment comment
+
+	static constraints = {
+		name(nullable:false, blank:false)
+		url(nullable:true, blank:true, url:true)
+		email(nullable:true, blank:true, email:true)
+	}
+   
 }
