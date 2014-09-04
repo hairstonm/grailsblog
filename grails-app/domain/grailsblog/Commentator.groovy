73ad4f16f7b67948effec9661f6acf6a2f940718
hairstonm <matthairston.79@gmail.com>
2014-09-04 16:31:58 -0400

    Added file tbhat was deleted

---
+++ grails-app/domain/grailsblog/Commentator.groovy
@@ -0,0 +1,20 @@
+package grailsblog
+	
+	class Commentator {
+		
+		static belongsTo = Comment
+	
+		String name
+		String url
+		String email
+		Comment comment
+	
+		static constraints = {
+			name(nullable:false, blank:false)
+			url(nullable:true, blank:true, url:true)
+			email(nullable:true, blank:true, email:true)
+		}
+	   
+	}
+
+
